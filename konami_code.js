03df705f1eb4e0cd4d5c7e11eec60a83c28469ec
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-24 22:35:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  document.body.addEventListener('keydown', onKeyDownHandle)
+
   let index = 0;
 
-  function onKeyDownHandler(e){
+  function onKeyDownHandle(e){
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]){
